c7b0a74f48cabc4f9ea59a62e044ffda4c72b6f8
Eliz <liezetta.246@gmail.com>
2025-11-23 02:13:17 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 function initmarquee(container){
-    const track = container.querySelector('.condition__marquee-track');
-    const inner = track.querySelector('.condition__marquee-inner');
-    const speed = parseInt(container.getAttribute('data-speed')) || 30;
-    const direction = container.getAttribute('data-direction') || 'left';
+const track = container.querySelector('.condition__marquee-track');
+const inner = track.querySelector('.condition__marquee-inner');
+const speed = parseInt(container.getAttribute('data-speed')) || 30;
+const direction = container.getAttribute('data-direction') || 'left';
 
     if (!inner.dataset.cloned){
         const originalBtn = Array.from(inner.children);
@@ -18,14 +18,18 @@ function initmarquee(container){
         inner.dataset.cloned = 'true';
     }
 
-    const updateAnimation = () =>{
-        const totalWidth = inner.scrollWidth/2;
-        const duration = totalWidth/speed;
-        inner.style.animationName = direction === 'right' ? 'scroll-right':
-        'scroll-left';
-        inner.style.animationDuration = `${duration}s`;
-    };
+const updateAnimation = () =>{
+    const totalWidth = inner.scrollWidth/2;
+    const duration = totalWidth/speed;
+    inner.style.animationName = direction === 'right' ? 'scroll-right':
+    'scroll-left';
+    inner.style.animationDuration = `${duration}s`;
+};
 
-    updateAnimation();
+updateAnimation();
     window.addEventListener('resize', updateAnimation);
 }
+
+window.onload = ()=>{
+    document.querySelectorAll('.condition__marquee-container').forEach(initmarquee);
+};
