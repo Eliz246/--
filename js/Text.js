fca43a0770c3248707231c61e87aadbf49828fb4
Eliz <liezetta.246@gmail.com>
2025-12-05 22:45:35 +0300

    last

--- js/Text.js
+++ js/Text.js
@@ -5,12 +5,10 @@ function updateHeadingText() {
     const mobileWidth = 350;
 
     if (window.innerWidth <= mobileWidth) {
-      // Если экран достаточно узкий, устанавливаем короткий текст
       if (headingElement.textContent !== mobileText) {
          headingElement.textContent = mobileText;
       }
     } else {
-      // Если экран широкий, возвращаем оригинальный текст
       if (headingElement.textContent !== originalText) {
         headingElement.textContent = originalText;
       }
